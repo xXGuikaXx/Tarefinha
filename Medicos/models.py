37cf40a1e089dd0542e2d2cdb630faa1c584679e
Guilherme Kawan <kawangui2007@gmail.com>
2025-05-23 00:36:10 -0300

    segunda real

--- Medicos/models.py
+++ Medicos/models.py
@@ -228,7 +228,7 @@ class Pacientes(models.Model):
             ('A-','A-'),
             ('B-','B-'),
             ('AB-','AB-'),
-            ('O-','O-'),
+            ('Z-','O-'),
         ]
     )
 
